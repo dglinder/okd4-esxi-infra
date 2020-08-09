b666b0e6395767c2d62c583cdc57c7e90da4e8c0
Daniel Linder <dan@linder.org>
2020-08-09 22:40:48 +0000

    Simple add/remove scripts until remote commands are fixed.

---
+++ terraform/setup-add.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -x
+
+export VAR_OKD_NETWORK="OKD"
+export VAR_VSWITCH="vSwitch0"
+export VAR_VLAN_ID=20
+
+echo Executing with portgroup-name=$VAR_OKD_NETWORK
+echo Executing with vswitch-name=$VAR_VSWITCH
+esxcli network vswitch standard portgroup add --portgroup-name=$VAR_OKD_NETWORK --vswitch-name=$VAR_VSWITCH
+echo EC-add:$?
+
