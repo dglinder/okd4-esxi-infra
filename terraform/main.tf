7ae3184bfd5bf3f2b5d1033f36951dffdc3acfaf
Daniel Linder <dan@linder.org>
2020-08-15 05:24:14 +0000

    Moving the provisioning into the build step

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,14 +87,11 @@ resource "esxi_guest" "okd4-bootstrap" {
   }
 
   notes = "Built using Terraform"
-
   clone_from_vm = "/Template-CentOS-8"
-
   #depends_on = [null_resource.esxi_network]
-
-}
-
-resource "null_resource" "remscript" {
+# }
+# 
+# resource "null_resource" "remscript" {
   provisioner "file" {
     connection {
       type  = "ssh"
